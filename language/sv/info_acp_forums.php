c58de6dc63220d76fc673f57e058610803ad9e6b
Maskinisten <info@gremminger.de>
2015-02-18 08:27:44 +0100

    Create info_acp_forums.php
    Swedish translation

---
+++ language/sv/info_acp_forums.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file is part of the phpBB Topic Solved extension package.
+ * Swedish translation by Holger (www.maskinisten.net)
+ *
+ * @copyright (c) Bryan Petty
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ * @package tierra/topicsolved/language
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ALLOW_SOLVE'               => 'Tillåt att ämnen markeras som lösta',
+	'ALLOW_SOLVE_EXPLAIN'       => 'Tillåter trådstartare och moderatorer att markera ett ämne som löst. Moderatorer kan markera ämnen med bägge Ja-inställningarna.',
+	'ALLOW_UNSOLVE'             => 'Tillåt att ämnen åter öppnas',
+	'ALLOW_UNSOLVE_EXPLAIN'     => 'Tillåter trådstartaren och moderatorer att återställa ämnet till olöst. Moderatorer kan återtälla ämnen till olöst med bägge Ja-inställningarna.',
+	'LOCK_SOLVED'               => 'Lås lösta ämnen',
+	'LOCK_SOLVED_EXPLAIN'       => 'Observera att endast moderatorer kan öppna låsta ämnen.',
+	'TOPIC_SOLVED_SETTINGS'     => 'Inställningar för lösta ämnen',
+	'FORUM_SOLVE_TEXT'          => 'Välj text istället för löst-grafik',
+	'FORUM_SOLVE_TEXT_EXPLAIN'  => 'Du kan använda text istället för löst-grafiken. T.ex. [LÖST] eller [SÅLD] eller något annat. Lämna tomt för att använda grafiken.',
+	'FORUM_SOLVE_COLOR'         => 'Textfärg',
+	'FORUM_SOLVE_COLOR_EXPLAIN' => 'Välj textfärg. Lämna tomt för att använda standardfärgen.',
+	'YES_MOD'                   => 'Ja, moderator',
+));
