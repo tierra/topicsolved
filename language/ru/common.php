1823e4d66394a3d1a621a3a987edca60ce8f079c
Anvar <anvar.all@mail.ru>
2015-05-26 00:10:11 +0600

    Update common.php

--- language/ru/common.php
+++ language/ru/common.php
@@ -19,13 +19,13 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'SEARCH_UNSOLVED'		=> 'Просмотреть нерешённые темы',
+	'SEARCH_UNSOLVED'			=> 'Просмотреть нерешённые темы',
 	'SEARCH_YOUR_UNSOLVED'		=> 'Просмотреть ваши нерешённые темы',
-	'SEARCH_SOLVED'			=> 'Искать только решённые темы',
-	'TOPIC_SOLVED'			=> 'Тема решена',
-	'SET_TOPIC_SOLVED'		=> 'Принять в качестве ответа',
+	'SEARCH_SOLVED'				=> 'Искать только решённые темы',
+	'TOPIC_SOLVED'				=> 'Тема решена',
+	'SET_TOPIC_SOLVED'			=> 'Принять в качестве ответа',
 	'SET_TOPIC_NOT_SOLVED'		=> 'Отметить тему как нерешённую',
-	'BAD_METHOD_CALL'		=> 'Неверный аргумент "%s".',
+	'BAD_METHOD_CALL'			=> 'Неверный аргумент "%s".',
 	'FORBIDDEN_MARK_SOLVED'		=> 'Вам не разрешено отмечать тему решённой или нерешённой.',
 	'TOPIC_ALREADY_SOLVED'		=> 'Тема уже помечена как решённая.',
 	'TOPIC_ALREADY_UNSOLVED'	=> 'Тема уже отмечена как нерешённая.',
