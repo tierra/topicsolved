a921fc69cfc71619cfbf8668f71c074e73d1b15d
Bryan Petty <bryan@ibaku.net>
2015-05-31 13:33:03 -0600

    Added first functional test to ensure ACP form works.

---
+++ tests/functional/acp_forums_test.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * This file is part of the phpBB Topic Solved extension package.
+ *
+ * @copyright (c) Bryan Petty
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ * @package tierra/topicsolved/tests/functional
+ */
+
+namespace tierra\topicsolved\tests\functional;
+
+/**
+ * @group functional
+ */
+class topicsolved_functional_acp_forums_test extends topicsolved_functional_base
+{
+	/**
+	 * Authenticate for testing ACP features.
+	 */
+	protected function forums_manage_login()
+	{
+		$this->login();
+		$this->admin_login();
+		$this->add_lang(array('acp/forums'));
+		//$this->add_lang_ext('tierra/topicsolved', array('info_acp_forums'));
+	}
+
+	/**
+	 * Return the URL for forum edit.
+	 */
+	protected function get_url()
+	{
+		return 'adm/index.php?i=acp_forums&mode=manage&action=edit';
+	}
+
+	/**
+	 * Test ACP forums form.
+	 */
+	public function test_acp_forums()
+	{
+		$this->forums_manage_login();
+		$crawler = self::request('GET', $this->get_url() . "&f=2&sid=" . $this->sid);
+
+		// Ensure extension and ACP language was loaded.
+		$this->assertContainsLang('ALLOW_SOLVE', $crawler->text());
+
+		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
+		$form->setValues(array(
+			'forum_allow_solve' => 1,
+			'forum_allow_unsolve' => 1,
+			'forum_lock_solved' => 1,
+			'forum_solve_text' => '[TEST]',
+			'forum_solve_color' => 'FF0000',
+		));
+		$crawler = self::submit($form);
+
+		// Form submission should be a success.
+		$this->assertContainsLang('FORUM_UPDATED', $crawler->text());
+	}
+}
---
+++ tests/functional/topicsolved_functional_base.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file is part of the phpBB Topic Solved extension package.
+ *
+ * @copyright (c) Bryan Petty
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ * @package tierra/topicsolved/tests/functional
+ */
+
+namespace tierra\topicsolved\tests\functional;
+
+/**
+ * @group functional
+ */
+class topicsolved_functional_base extends \phpbb_functional_test_case
+{
+	/**
+	 * Define the extensions to be tested
+	 *
+	 * @return array vendor/name of extension(s) to test
+	 */
+	static protected function setup_extensions()
+	{
+		return array('tierra/topicsolved');
+	}
+
+	/**
+	 * Initialize extension for tests.
+	 */
+	public function setUp()
+	{
+		parent::setUp();
+		$this->purge_cache();
+		$this->add_lang_ext('tierra/topicsolved', array(
+			'common',
+			'info_acp_forums',
+			'info_acp_styles',
+		));
+	}
+}
