86b5cdde91641fe41c3f869b2bec514b35ee5633
R3gi <regiprogi@gmail.com>
2015-06-03 16:30:34 +0200

    Update info_acp_styles.php
    Fixed and improved

--- language/cs/info_acp_styles.php
+++ language/cs/info_acp_styles.php
@@ -22,6 +22,6 @@ $lang = array_merge($lang, array(
 	'IMG_ICON_TOPIC_SOLVED_HEAD'    => 'Téma je vyřešené',
 	'IMG_ICON_TOPIC_SOLVED_LIST'    => 'Téma je vyřešené',
 	'IMG_ICON_TOPIC_SOLVED_POST'    => 'Téma je vyřešené',
-	'IMG_ICON_TOPIC_SOLVED_SET'     => 'Potvrďte tuto odpovčď jako řešení tématu',
-	'IMG_ICON_TOPIC_SOLVED_UNSET'   => 'Zrušte označení tématu jako vyřešené',
+	'IMG_ICON_TOPIC_SOLVED_SET'     => 'Nastavit tento příspěvek jako řešení tématu',
+	'IMG_ICON_TOPIC_SOLVED_UNSET'   => 'Zrušit označení tématu jako vyřešené',
 ));
