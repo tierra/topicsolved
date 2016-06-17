feb231975d366993a96f4e1b373e3b41d5f8682e
Daniel Mota <leinad4mind@gmail.com>
2016-06-17 02:52:42 +0100

    Fix portuguese language
    It was a very bad translation, fixed

--- language/pt/info_acp_topicsolved.php
+++ language/pt/info_acp_topicsolved.php
@@ -19,17 +19,17 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'ALLOW_SOLVE'                => 'Permitir que  tópicos podem ser marcados como resolvidos.',
-	'ALLOW_SOLVE_EXPLAIN'        => 'Deixa o dono do tópico (quem o iniciou) ou os moderadores marcarem o tópico como resolvido. Moderadores podem resolver tópicos em ambas as opções possitiva.',
+	'ALLOW_SOLVE'                => 'Permitir que os tópicos sejam marcados como resolvidos.',
+	'ALLOW_SOLVE_EXPLAIN'        => 'Permite ao autor do tópico ou aos moderadores marcar o tópico como resolvido. Os moderadores podem resolver tópicos em ambas as opções positivas.',
 	'ALLOW_UNSOLVE'              => 'Permitir que os tópicos possam ser reabertos',
-	'ALLOW_UNSOLVE_EXPLAIN'      => 'Permite aos usuários ou moderadores retornar um tópico para não resolvido. Moderadores podem desfazer a resolução dos tópicos em ambas as opções possitiva.',
-	'LOCK_SOLVED'                => 'Tranca tópicos resolvidos',
-	'LOCK_SOLVED_EXPLAIN'        => 'Apenas os moderadores podem reabrir tópicos trancados.',
+	'ALLOW_UNSOLVE_EXPLAIN'      => 'Permite aos utilizadores ou aos moderadores alterar o estado de um tópico para não resolvido. Os moderadores podem alterar o estado dos tópicos em ambas as opções positivas.',
+	'LOCK_SOLVED'                => 'Bloquear tópicos resolvidos',
+	'LOCK_SOLVED_EXPLAIN'        => 'Apenas os moderadores podem reabrir tópicos bloqueados.',
 	'TOPIC_SOLVED_SETTINGS'      => 'Configurações dos tópicos resolvidos',
-	'FORUM_SOLVE_TEXT'           => 'Escolha um texto em vez de uma imagem para resolvido',
-	'FORUM_SOLVE_TEXT_EXPLAIN'   => 'Você pode usar um texto, em vez de uma imagem, para marcar um tópico resolvido. Ex.: [RESOLVIDO] or [VENDIDO] ou qualquer outra coisa. Deixe em branco para usar a imagem padrão.',
+	'FORUM_SOLVE_TEXT'           => 'Escolha um texto em vez de uma imagem para marcar como resolvido',
+	'FORUM_SOLVE_TEXT_EXPLAIN'   => 'Podes usar um texto, em vez de uma imagem, para marcar um tópico como resolvido. Ex: [RESOLVIDO] ou [VENDIDO] ou qualquer outra coisa. Deixe em branco para usar a imagem  por defeito.',
 	'FORUM_SOLVE_COLOUR'         => 'Cor do texto',
-	'FORUM_SOLVE_COLOUR_EXPLAIN' => 'Escolha a cor do texto. Deixe em branco para usar a cor padrão.',
+	'FORUM_SOLVE_COLOUR_EXPLAIN' => 'Escolha a cor do texto. Deixe em branco para usar a cor por defeito.',
 	'YES_MOD'                    => 'Sim, moderador',
 
 	'IMG_ICON_TOPIC_SOLVED_HEAD'    => 'Tópico resolvido',
