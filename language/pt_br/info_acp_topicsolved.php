bf7b4ec35f549c76b8d37c6094075da6f044cd93
Daniel Mota <leinad4mind@gmail.com>
2016-06-17 02:54:57 +0100

    Update info_acp_topicsolved.php

--- language/pt_br/info_acp_topicsolved.php
+++ language/pt_br/info_acp_topicsolved.php
@@ -20,14 +20,14 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'ALLOW_SOLVE'                => 'Habilita tópicos a serem marcados como resolvidos.',
-	'ALLOW_SOLVE_EXPLAIN'        => 'Deixa o dono do tópico (quem o iniciou) ou os moderadores marcarem o tópico como resolvido. Moderadores podem resolver tópicos em ambas as opções positivas.',
+	'ALLOW_SOLVE_EXPLAIN'        => 'Deixa o autor do tópico ou os moderadores marcarem o tópico como resolvido. Moderadores podem resolver tópicos em ambas as opções positivas.',
 	'ALLOW_UNSOLVE'              => 'Habilita os tópicos a serem reabertos',
 	'ALLOW_UNSOLVE_EXPLAIN'      => 'Permite aos usuários ou moderadores retornar um tópico para não resolvido. Moderadores podem desfazer a resolução dos tópicos em ambas as opções positivas.',
 	'LOCK_SOLVED'                => 'Tranca tópicos resolvidos',
 	'LOCK_SOLVED_EXPLAIN'        => 'Apenas os moderadores podem reabrir tópicos trancados.',
 	'TOPIC_SOLVED_SETTINGS'      => 'Configurações dos tópicos resolvidos',
 	'FORUM_SOLVE_TEXT'           => 'Escolha um texto em vez de uma imagem para resolvido',
-	'FORUM_SOLVE_TEXT_EXPLAIN'   => 'Você pode usar um texto, em vez de uma imagem, para marcar um tópico resolvido. Ex.: [RESOLVIDO] or [VENDIDO] ou qualquer outra coisa. Deixe em branco para usar a imagem padrão.',
+	'FORUM_SOLVE_TEXT_EXPLAIN'   => 'Você pode usar um texto, em vez de uma imagem, para marcar um tópico resolvido. Ex: [RESOLVIDO] ou [VENDIDO] ou qualquer outra coisa. Deixe em branco para usar a imagem padrão.',
 	'FORUM_SOLVE_COLOUR'         => 'Cor do texto',
 	'FORUM_SOLVE_COLOUR_EXPLAIN' => 'Escolha a cor do texto. Deixe em branco para usar a cor padrão.',
 	'YES_MOD'                    => 'Sim, moderador',
